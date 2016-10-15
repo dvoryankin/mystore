6edef4389dcc2368aaecd5824740df4195f7bc70
pchellovod <pchellovod@gmail.com>
2016-10-15 20:10:38 +0500

    add params to item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,17 @@ class ItemsController < ApplicationController
     render text: @items.map { |i| "#{i.name}: #{i.price}"}.join('<br/>')
   end
 
+  def create
+
+    @item = Item.create(name: params[:name],
+                description: params[:description],
+                price: params[:real],
+                real: params[:real],
+                weight: params[:weight])
+    render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+  end
+
 end
+
+
+#localhost:3000/items/create?item[name]=car33&item[description]=good+car&item[price]=500000&item[weight]=3&item[real]=1
