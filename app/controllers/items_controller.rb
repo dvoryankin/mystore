8218189d70403b85e267aa1bfa960e1b80adc004
pchellovod <pchellovod@gmail.com>
2016-10-16 00:33:56 +0500

    add actions and views create and new

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,20 +2,55 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    render text: @items.map { |i| "#{i.name}: #{i.price}"}.join('<br/>')
   end
 
+  # /items/id1 GET
+  def show
+    unless @item = Item.where(id: params[:id]).first
+      render text: "Page not found", status: 404
+    end
+  end
+
+  # /items/new GET
+  def new
+    @item = Item.new
+  end
+
+  # /items/id1/edit GET
+  def edit
+  end
+
+  # /items POST
   def create
-
-    @item = Item.create(name: params[:name],
-                description: params[:description],
-                price: params[:real],
-                real: params[:real],
-                weight: params[:weight])
-    render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+    item_params = params.require(:item).permit(:price, :name, :real, :weight, :description)
+    @item = Item.create(item_params)
+    # @item = Item.create(params[:item])
+    if @item.errors.empty?
+      redirect_to item_path(@item)
+    else
+      render "new"
+    end
   end
+
+  # /items/1 PUT
+  def update
+  end
+
+  # /items/1 DELETE
+  def destroy
+  end
+
+
 
 end
 
 
 #localhost:3000/items/create?item[name]=car33&item[description]=good+car&item[price]=500000&item[weight]=3&item[real]=1
+
+# @item = Item.create(name: params[:name],
+#                     description: params[:description],
+#                     price: params[:real],
+#                     real: params[:real],
+#                     weight: params[:weight])
+
+# render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
